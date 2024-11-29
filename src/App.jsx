3ff032c2ffd4e236292ef406f1eb601012f3361d
aliji865030 <abbasalichand786@gmail.com>
2024-11-29 12:39:13 +0530

    final final

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import Nav from './Components/Nav'
 import HeroSection from './Components/HeroSection'
 import Faq from './Components/Faq'
-import Blog from './Components/blog'
 import Footer from './Components/Footer'
 import Blogs from './Components/Blogs'
 
